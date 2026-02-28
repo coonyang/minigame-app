30a6d3d16fcf3086b6605d8d218da60a88bd5a23
Hajun Jeon <gkwns1231@naver.com>
2026-03-01 04:58:04 +0900

    인라인 스타일 css로 이동

--- src/pages/MiniGamePage.jsx
+++ src/pages/MiniGamePage.jsx
@@ -2,18 +2,23 @@ import { Link, Outlet } from "react-router-dom";
 
 export default function MiniGamePage() {
   return (
-    <div style={{ textAlign: "center", color: "white" }}>
-      <h2>미니게임 목록</h2>
-      <nav style={{ display: "flex", gap: "10px", justifyContent: "center" }}>
-        <Link to="수박게임" style={{ color: "cyan" }}>
-          수박게임
+    <div className="minigame-container">
+      <h2 className="minigame-title">명조 미니 게임</h2>
+
+      <nav className="minigame-nav">
+        <Link to="수박게임" className="game-link">
+          수박 게임
         </Link>
-        <Link to="새알심쌓기" style={{ color: "cyan" }}>
-          새알심쌓기
+        <Link to="새알심쌓기" className="game-link">
+          새알심 쌓기
         </Link>
       </nav>
-      <hr />
-      <Outlet />
+
+      <hr className="minigame-divider" />
+
+      <div className="game-viewport">
+        <Outlet />
+      </div>
     </div>
   );
 }
